a829d3ea9d4617382d194a43194e9e23362ddfbb
chjcago2001 <88874679+chjcago2001@users.noreply.github.com>
2023-07-06 23:47:23 +0700

    feat: update apollo client

--- client/src/graphql/apollo-client.ts
+++ client/src/graphql/apollo-client.ts
@@ -1,11 +1,46 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createClient } from 'graphql-ws';
+import { getSession } from 'next-auth/react';
+
+const wsLink =
+    typeof window !== 'undefined'
+        ? new GraphQLWsLink(
+              createClient({
+                  url:
+                      process.env.NEXT_PUBLIC_WS_GRAPHQL_ENDPOINT ??
+                      'ws://localhost:5000/graphql/subscriptions',
+                  connectionParams: async () => ({
+                      session: await getSession(),
+                  }),
+              })
+          )
+        : null;
 
 const httpLink = new HttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+    uri:
+        process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ??
+        `http://localhost:5000/graphql`,
     credentials: 'include',
 });
 
+const link =
+    typeof window !== 'undefined' && wsLink != null
+        ? split(
+              ({ query }) => {
+                  const def = getMainDefinition(query);
+                  return (
+                      def.kind === 'OperationDefinition' &&
+                      def.operation === 'subscription'
+                  );
+              },
+              wsLink,
+              httpLink
+          )
+        : httpLink;
+
 export const client = new ApolloClient({
-    link: httpLink,
+    link,
     cache: new InMemoryCache(),
 });
