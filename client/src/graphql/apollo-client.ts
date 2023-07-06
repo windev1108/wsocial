c07b3fbc9fa6971bc5129678a65d58e7312cb9e9
chjcago2001 <88874679+chjcago2001@users.noreply.github.com>
2023-07-06 23:16:55 +0700

    fix: update env graphql

--- client/src/graphql/apollo-client.ts
+++ client/src/graphql/apollo-client.ts
@@ -1,14 +1,11 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-
-
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: `http://localhost:5000/graphql`,
-  credentials: "include",
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+    credentials: 'include',
 });
 
-
 export const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+    link: httpLink,
+    cache: new InMemoryCache(),
 });
